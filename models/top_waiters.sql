72ac838bfa037779ef9a15dd2f0d7bdce3715122
Jian TANG <jtang1126@gmail.com>
2025-05-02 19:42:10 +0100

    clickhouse model with multiple gateways

--- models/top_waiters.sql
+++ models/top_waiters.sql
@@ -1,23 +1,28 @@
 -- View of 10 waiters with highest revenue on most recent day of data.
 MODEL (
   name sushisimple.top_waiters,
-  kind VIEW,
+  kind FULL,
   cron '@daily',
-  grain waiter_id
+  physical_properties (
+        order_by = (waiter_id),
+        allow_nullable_key = 1  
+  ),
+  gateway tangram_clickhouse
 );
+
 
 SELECT
   waiter_id::INT AS waiter_id,
   name::TEXT AS waiter_name,
   revenue::DOUBLE AS revenue
-FROM sushisimple.waiter_revenue_by_day as r
-LEFT JOIN sushisimple.waiter_names AS n
+FROM iceberg.sushisimple.waiter_revenue_by_day as r
+LEFT JOIN iceberg.sushisimple.waiter_names AS n
   ON r.waiter_id = n.id
 WHERE
   ds = (
     SELECT
       MAX(ds)
-    FROM sushisimple.waiter_revenue_by_day
+    FROM iceberg.sushisimple.waiter_revenue_by_day
   )
 ORDER BY
   revenue DESC
